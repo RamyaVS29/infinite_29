6bb8ee30deb4c4517b023e0e8d98931c55622363
Ramya Sathish <ramyas@infinite.com>
2024-03-11 18:26:26 +0530

    myassignment

---
+++ csharp/Assignment csharp/assignment_4/Concession/Concession/lambda1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class lambda1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter numbers :");
+        string input = Console.ReadLine();
+        List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+        var result = numbers.Select(n => new { Number = n, Square = n * n })
+                            .Where(x => x.Square > 20)
+                            .ToList();
+
+        foreach (var item in result)
+        {
+            Console.WriteLine($"Number: {item.Number}, Square: {item.Square}");
+        }
+
+        Console.Read();
+    }
+}
---
+++ csharp/Assignment csharp/assignment_4/Concession/Concession/lambda2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class lambda2
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a list of words separated by commas:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(',');
+
+        List<string> filteredWords = new List<string>();
+        foreach (string word in words)
+        {
+            if (word.StartsWith("a", StringComparison.OrdinalIgnoreCase) &&
+                word.EndsWith("m", StringComparison.OrdinalIgnoreCase))
+            {
+                filteredWords.Add(word);
+            }
+        }
+
+        Console.WriteLine("Words starting with 'a' and ending with 'm':");
+        foreach (string filteredWord in filteredWords)
+        {
+            Console.WriteLine(filteredWord);
+            Console.ReadLine();
+        }
+    }
+}
